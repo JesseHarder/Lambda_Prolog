7ef430b9ee4a81330166c948f2ef47a2a4445f1a
JesseHarder <jhharder@scu.edu>
2017-11-02 14:17:51 -0700

    First commit

---
+++ types.pl
@@ -0,0 +1,17 @@
+/*
+ * Prolog code to be used to write type-checkable prolog code.
+ *
+ * Not, existing type checking predicates in prolog are listed here:
+ * http://pauillac.inria.fr/~haemmerl/gprolog-rh/doc/manual024.html
+ *
+ */
+
+/* Implemented Types:
+ * int
+ */
+
+/* Predicate: type(Term, Type)
+ * Meaning: Term "Term" has type "Type".
+ */
+
+ type(Term, int) :
