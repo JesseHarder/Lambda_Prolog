f9a60e99933d81128b207df8bb25eb93ee3b4bc5
JesseHarder <jhharder@scu.edu>
2017-11-11 18:21:06 -0800

    Modified type predicate comments to "->" syntax.

--- types.pl
+++ types.pl
@@ -23,33 +23,32 @@
 
  /* ----- Predicate Types ----- */
  /* -- Booleans -- */
- % ifthenelse: [bool, T, T, T]
+ % ifthenelse: bool -> T -> T -> T
  type(ifthenelse(A,B,C,D),[bool,T2,T2,T2]) :-
     ifthenelse(A,B,C,D),
     type(A, bool),
   	type(B, T2),
   	type(C, T2),
   	type(D, T2).
-
- % and: [bool, bool, bool]
+ % and: bool -> bool -> bool
  type(and(X,Y,Z),[bool,bool,bool]) :-
     and(X,Y,Z),
     type(X, bool),
  	type(Y, bool),
  	type(Z, bool).
- % or: [bool, bool, bool]
+ % or: bool -> bool -> bool
  type(or(X,Y,Z),[bool,bool,bool]) :-
     or(X,Y,Z),
  	type(X, bool),
  	type(Y, bool),
  	type(Z, bool).
- % xor: [bool, bool, bool]
+ % xor: bool -> bool -> bool
  type(xor(X,Y,Z),[bool,bool,bool]) :-
     xor(X,Y,Z),
  	type(X, bool),
  	type(Y, bool),
  	type(Z, bool).
- % not: [bool, bool]
+ % not: bool -> bool
  type(not(X,Y),[bool,bool]) :-
     not(X,Y),
  	type(X, bool),
@@ -60,40 +59,40 @@
 /* TODO: Figure out why the following four run forever if asked to check
  * for a second result
  */
-% succ: [number, number]
+% succ: number -> number
 type(succ(X,Y),[number,number]) :-
     succ(X,Y),
 	type(X, T1),
 	type(Y, T1),
 	T1 = number.
-% pred: [number, number]
+% pred: number -> number
 type(pred(X,Y),[number,number]) :-
     pred(X,Y),
 	type(X, T1),
 	type(Y, T1),
 	T1 = number.
-% add: [number, number, number]
+% add: number -> number -> number
 type(add(X,Y,S),[number,number,number]) :-
     add(X,Y,S),
 	type(X, T1),
 	type(Y, T1),
 	type(S, T1),
 	T1 = number.
-% sub: [number, number, number]
+% sub: number -> number -> number
 type(sub(X,Y,D),[number,number,number]) :-
     sub(X,Y,D),
 	type(X, T1),
 	type(Y, T1),
 	type(D, T1),
 	T1 = number.
-% mul: [number, number, number]
+% mul: number -> number -> number
 type(mul(X,Y,P),[number,number,number]) :-
     mul(X,Y,P),
 	type(X, T1),
 	type(Y, T1),
 	type(P, T1),
 	T1 = number.
-% div: [number, number, number]
+% div: number -> number -> number
 type(div(N,D,Q),[number,number,number]) :-
     div(N,D,Q),
 	type(N, T1),
