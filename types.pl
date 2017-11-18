ec02193b2f6ffd55e295760880d24b293c68362a
JesseHarder <jhharder@scu.edu>
2017-11-18 15:28:21 -0800

    Commented out old type rules from first attempt.

--- types.pl
+++ types.pl
@@ -1,5 +1,4 @@
-'Natural'/*
- * Prolog code to be used to write type-checkable prolog code.
+/* Lambda calculuc implemented in prolog.
  *
  * Note, existing type checking predicates in prolog are listed here:
  * http://pauillac.inria.fr/~haemmerl/gprolog-rh/doc/manual024.html
@@ -9,21 +8,6 @@
  /* Included Labda calculus features:
   *     Booleans
   *     Numbers
-  *     Unit
-  *     Tuples (built on top of lists.)
-  *     Lists (using built in list type.)
-  *
-  * Derived featurs:
-  *     Application! - Turns out this is already in Prolog. See application.txt
-  *         for a more detailed description.
-  *     Let - this is basically just Prolog's = operator. If I write something
-  *         like X = A+B*C, X is not the same thing as A+B*C, unevevaluated.
-  *     Ascription - this is basically just adding a note of a new type. See
-  *         the Ascription comment below for where these should go and how
-  *         they should look.
-  *     Sums and Variants - Already built in? Just write same predicate to
-  *         work for different type inputs?
-  *     Fix - Not needed? Prolog is already recursive.
   */
 
 :- [util/plists].
@@ -41,9 +25,9 @@ types_in_list([Head|Tail], [T_Head|T_Tail]) :-
 type('Unit').
 type('Bool').
 type('Natural').
-type('List'(T)) :- type(T).
-type('Tuple'([H])) :- type(H).
-type('Tuple'([H|T])) :- type(H), type('Tuple'(T)).
+% type('List'(T)) :- type(T).
+% type('Tuple'([H])) :- type(H).
+% type('Tuple'([H|T])) :- type(H), type('Tuple'(T)).
 % Function Type: where [T1, T2, T3] is T1 -> T2 -> T3.
 type([T]) :- type(T).
 type([H|T]) :- type(H),type(T).
@@ -72,16 +56,16 @@ typeof(pred(X), 'Natural') :- typeof(X, 'Natural'). % T-Succ
 typeof(iszero(X), 'Bool') :- typeof(X, 'Natural'). % T-IsZero
 
 /***** Lists *****/
-typeof([],'List'(T)) :- type(T). % Empty list can be list of any type.
-typeof([Head|Tail],'List'(T)) :-   % A list has type list of T's if
-     typeof(Head, T),             % the fisrt element has type T and
-     typeof(Tail, 'List'(T)).       % the tail is a list of T's.
+% typeof([],'List'(T)) :- type(T). % Empty list can be list of any type.
+% typeof([Head|Tail],'List'(T)) :-   % A list has type list of T's if
+%      typeof(Head, T),             % the fisrt element has type T and
+%      typeof(Tail, 'List'(T)).       % the tail is a list of T's.
 
 /***** Tuples *****/
-typeof(tuple([Val]), 'Tuple'([T])) :- typeof(Val,T).
-typeof(tuple(List), 'Tuple'(Types)) :-
-    is_list(List), length(List, L), L > 0, % "Lists" is a non-empty list.
-    types_in_list(List,Types).
+% typeof(tuple([Val]), 'Tuple'([T])) :- typeof(Val,T).
+% typeof(tuple(List), 'Tuple'(Types)) :-
+%     is_list(List), length(List, L), L > 0, % "Lists" is a non-empty list.
+%     types_in_list(List,Types).
 
 /***** Variables *****
  * Variables can be of any type.
