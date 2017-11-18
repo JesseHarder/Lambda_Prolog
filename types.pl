40f3dce048d3a0228fef47a4d5092141268bc3f6
JesseHarder <jhharder@scu.edu>
2017-11-18 14:23:38 -0800

    More restructuring of types.pl.

--- types.pl
+++ types.pl
@@ -34,7 +34,7 @@ types_in_list([Head|Tail], [T_Head|T_Tail]) :-
     type(Head, T_Head),
     types_in_list(Tail, T_Tail).
 
-/* ----- Listing Valid Types -----
+/* ---------- Listing Valid Types ----------
  * This section is like the "T::=..." section of our syntax.
  */
 
@@ -52,11 +52,11 @@ type([H|T]) :- type(H),type(T).
 
 
 
-/* ----- Typing Rules ----- */
-% Unit type
+/* ---------- Typing Rules ---------- */
+/***** Unit type *****/
 typeof(unit, 'Unit').
 
-% Booleans
+/***** Booleans *****/
 typeof(tru, 'Bool'). % T-True
 typeof(fls, 'Bool'). % T-False
 % T-If
@@ -65,28 +65,31 @@ typeof(ifte(Term1,Term2,Term3), Type) :-
     typeof(Term2,Type),
     typeof(Term3,Type).
 
-% Numbers
+/***** Numbers *****/
 typeof(0, 'Natural').   % T-Zero
 typeof(succ(X), 'Natural') :- typeof(X, 'Natural'). % T-Succ
 typeof(pred(X), 'Natural') :- typeof(X, 'Natural'). % T-Succ
 typeof(iszero(X), 'Bool') :- typeof(X, 'Natural'). % T-IsZero
 
-% Lists
+/***** Lists *****/
 typeof([],'List'(T)) :- type(T). % Empty list can be list of any type.
 typeof([Head|Tail],'List'(T)) :-   % A list has type list of T's if
      typeof(Head, T),             % the fisrt element has type T and
      typeof(Tail, 'List'(T)).       % the tail is a list of T's.
-% Tuples
+
+/***** Tuples *****/
 typeof(tuple([Val]), 'Tuple'([T])) :- typeof(Val,T).
 typeof(tuple(List), 'Tuple'(Types)) :-
     is_list(List), length(List, L), L > 0, % "Lists" is a non-empty list.
     types_in_list(List,Types).
+
+/***** Variables *****
+ * Variables can be of any type.
+ */
+typeof(Var, Type) :- var(Var), type(Type).
 
 /* Ascriptions - Add ascription below this comment of the form:
  *      typeof(X, <NewTypeName>) :- typeof(X, <OldTypeRepresentation>).
  *  Example:
  *      typeof(X, 'NNN') :- typeof(X, ['Natural','Natural','Natural']).
  */
-
-/* Variables - can be of any type. */
-typeof(Var, Type) :- var(Var), type(Type).
