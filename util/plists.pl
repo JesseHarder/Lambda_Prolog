d794caaccd4d18eba59fe2b245998d929ddaa628
JesseHarder <jhharder@scu.edu>
2017-11-19 23:52:47 -0800

    Added is_in/2 to plist.pl.

--- util/plists.pl
+++ util/plists.pl
@@ -3,8 +3,12 @@
  * in any way other than that they share a representation.
  */
 
-/* Function for getting the ith element of a list. */
+/* Function for getting the ith element of a list, with starting index of 1. */
 ith_elm([Head|_], 1, Head).
 ith_elm([_|Tail], I, Elm) :-
 	I > 1, J is I-1,
-	ith_elm(Tail, J, Elm).
+ 	ith_elm(Tail, J, Elm).
+
+/* Resolves if X is an element in the list given as second term. */
+is_in(X, [X|_]).
+is_in(X, [_|Tail]) :- is_in(X,Tail).
