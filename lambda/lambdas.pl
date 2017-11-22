4801455ffedf8c767e7815a37f9ff7dbd9535031
JesseHarder <jhharder@scu.edu>
2017-11-21 20:14:45 -0800

    Added is_lambda/1 and apply/3 versions for typed lambdas.
    Confirmed that eval() now works for typed abstractions as well.

--- lambda/lambdas.pl
+++ lambda/lambdas.pl
@@ -1,13 +1,24 @@
-% Lambdas
+/* --- is_lambda check --- */
+% Untyped Lambda Calculus
 is_lambda(lam(X,SubTerms)) :-	% To be an abstraction you must have
-	var(X),					% unbound variable X and
-	is_list(SubTerms).		% a list of subterms, possibly containing X.
+	var(X),						% unbound variable X and
+	is_list(SubTerms),!.		% a list of subterms, possibly containing X.
+% Simply Typed Lambda Calculus
+is_lambda(lam(X:T,SubTerms)) :-
+	type(T), is_lambda(lam(X,SubTerms)).
 
+/* --- application evaluation --- */
+% Untyped Lambda Calculus
 apply(lam(X,[SubTerm]),Y,Result) :-	% Performing an application requires that
+	var(X),
 	is_lambda(lam(X,[SubTerm])), 	% the first term be a valid abstraction
 	X = Y,						% the unbound variable be equal the second term.
 	Result = SubTerm,!.
 apply(lam(X,SubTerms),Y,Result) :-	% Performing an application requires that
+	var(X),
 	is_lambda(lam(X,SubTerms)), 	% the first term be a valid abstraction
 	X = Y,						% the unbound variable be equal the second term.
-	Result = SubTerms.
+	Result = SubTerms,!.
+% Simply Typed Lambda Calculus
+apply(lam(X:T,SubTerms),Y,Result) :-
+	type(T), apply(lam(X,SubTerms),Y,Result).
