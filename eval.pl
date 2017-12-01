1ba14bbabee951891422c41b6581195cd4266628
JesseHarder <jhharder@scu.edu>
2017-12-01 13:44:34 -0800

    Changed every predicate to end with './run.sh input/Preferences.csv '

--- eval.pl
+++ eval.pl
@@ -19,55 +19,55 @@ eval_if_not_value(Term, Result) :-
 		% If Term is a value, it is the result.
 		Result = Term;
 		% If not, the result of evaluating it is the result.
-		eval(Term, Result)).
+		eval(Term, Result)),!.
 
 
 /* --- Booelean Evaluation --- */
 %E-IfTrue
 eval(ifte(tru, Term1, _),Result) :-
-	eval_if_not_value(Term1,Result).
+	eval_if_not_value(Term1,Result),!.
 %E-IfFalse
 eval(ifte(fls, _, Term2),Result) :-
-	eval_if_not_value(Term2,Result).
+	eval_if_not_value(Term2,Result),!.
 %E-If
 eval(ifte(Term1, Term2, Term3),Result) :-
 	eval(Term1, New1),
-	eval_if_not_value(ifte(New1, Term2, Term3),Result).
+	eval_if_not_value(ifte(New1, Term2, Term3),Result),!.
 
 
 /* --- Natural Number Evaluation --- */
 % E-PredZero
-eval(pred(0),0) :- !.
+eval(pred(0), 0) :- !.
 % E-PredSucc
-eval(pred(succ(X)),X) :-
+eval(pred(succ(X)), X) :-
 	is_natural_value(X),!.
 % E-Succ
 eval(succ(Term),Result) :-
 	eval(Term,NewTerm),
 	eval_if_not_value(succ(NewTerm),Result),!.
 % E-Pred
-eval(pred(Term),Result) :-
-	eval(Term,NewTerm),
-	eval_if_not_value(pred(NewTerm),Result),!.
+eval(pred(Term), Result) :-
+	eval(Term, NewTerm),
+	eval_if_not_value(pred(NewTerm), Result),!.
 % E-IsZeroZero
-eval(iszero(0),tru).
+eval(iszero(0),tru),!.
 % E-IsZeroSucc
 eval(iszero(succ(X)),fls) :-
-	is_natural_value(X).
+	is_natural_value(X),!.
 % E-IsZero
 eval(iszero(Term),Result) :-
 	eval(Term,NewTerm),
-	eval(iszero(NewTerm),Result).
+	eval(iszero(NewTerm),Result),!.
 
 
 /* --- Sequences with Unit type ---*/
 % This doesn't currently use lambda to perform the eval sequence.
 % TODO: Check this with Cormac.
 eval(seq([Term]),Result) :-
-	eval(Term,Result).
+	eval(Term,Result),!.
 eval(seq([FirstTerm|OtherTerms]),Result) :-
 	eval(FirstTerm,_),
-	eval(seq(OtherTerms),Result).
+	eval(seq(OtherTerms),Result),!.
 
 
 /* --- Let --- */
@@ -76,12 +76,12 @@ eval(seq([FirstTerm|OtherTerms]),Result) :-
 eval(let(X,Val,Term2),Result) :-
 	var(X),is_value(Val),
 	X=Val,
-	eval(Term2,Result).
+	eval(Term2,Result),!.
 % E-Let
 eval(let(X,Term1,Term2),Result) :-
 	var(X),is_not_value(Term1),
 	eval(Term1,New1),
-	eval(let(X,New1,Term2),Result).
+	eval(let(X,New1,Term2),Result),!.
 
 
 /* --- Tuples --- */
@@ -89,15 +89,15 @@ eval(let(X,Term1,Term2),Result) :-
 eval(proj(tuple(List),Index),Result) :-
 	is_value(tuple(List)),
 	is_list(List), length(List,Len), Index >= 1, Index =< Len, % Sanity Check
-	ith_elm(Index,List,Result).
+	ith_elm(Index,List,Result),!.
 % E-Proj
 eval(proj(tuple(List),Index), Result) :-
 	eval(tuple(List), NewTuple),
-	eval(proj(NewTuple,Index), Result).
+	eval(proj(NewTuple,Index), Result),!.
 % E-Tuple - This is sort of the Big-Step version of this.
 eval(tuple(List), tuple(Vals)) :-
 	is_not_value(tuple(List)),
-	maplist(eval_if_not_value,List,Vals).
+	maplist(eval_if_not_value,List,Vals),!.
 
 
 /* --- Records --- */
@@ -105,56 +105,56 @@ eval(tuple(List), tuple(Vals)) :-
 eval(proj(record(List),Label),Result) :-
 	is_value(record(List)),
 	is_list(List), string(Label), % Sanity Check
-	member(Label=Result,List).
+	member(Label=Result,List),!.
 % E-Proj
 eval(proj(record(List),Label), Result) :-
 	is_not_value(record(List)),	% Sanity check.
 	eval(record(List), NewRecord),
-	eval(proj(NewRecord,Label), Result).
+	eval(proj(NewRecord,Label), Result),!.
 % E-Rcd
 eval(record(List), record(NewList)) :-
 	is_not_value(record(List)),
 	record_parts(record(List),Labels,Terms),
 	maplist(eval_if_not_value,Terms,Vals),
-	record_parts(record(NewList),Labels,Vals).
+	record_parts(record(NewList),Labels,Vals),!.
 
 
 /* --- Lists --- */
 % E-Cons1
 eval(cons(Term1, Term2), Result) :-
 	eval(Term1, New1),
-	eval_if_not_value(cons(New1, Term2), Result).
+	eval_if_not_value(cons(New1, Term2), Result),!.
 % E-Cons2
 eval(cons(Val1, Term2), Result) :-
 	is_value(Val1),
 	eval(Term2, New2),
-	eval_if_not_value(cons(Val1, New2), Result).
+	eval_if_not_value(cons(Val1, New2), Result),!.
 % E-IsNilNil
-eval(isnil(nil), tru).
+eval(isnil(nil), tru),!.
 % E-IsNilCons
 eval(isnil(cons(V1, V2)), fls) :-
 	is_value(V1),
-	is_value(V2).
+	is_value(V2),!.
 % E-IsNil
 eval(isnil(Term), Result) :-
 	eval(Term, NewTerm),
-	eval_if_not_value(isnil(NewTerm), Result).
+	eval_if_not_value(isnil(NewTerm), Result),!.
 % E-HeadCons
 eval(head(cons(V1, V2)), V1) :-
 	is_value(V1),
-	is_value(V2).
+	is_value(V2),!.
 % E-Head
 eval(head(Term), Result) :-
 	eval(Term, NewTerm),
-	eval(head(NewTerm), Result).
+	eval(head(NewTerm), Result),!.
 % E-TailCons
 eval(tail(cons(V1, V2)), V2) :-
 	is_value(V1),
-	is_value(V2).
+	is_value(V2),!.
 % E-Tail
 eval(tail(Term), Result) :-
 	eval(Term, NewTerm),
-	eval(tail(NewTerm), Result).
+	eval(tail(NewTerm), Result),!.
 
 
 /* --- Variant Types: Case ---
@@ -178,7 +178,7 @@ eval(case(var(Label=Val), Conditions), Result) :-
 % E-Case
 eval(case(var(Label=Term), Conditions), Result) :-
 	eval(var(Label=Term), NewLabelTerm),
-	eval(case(NewLabelTerm, Conditions), Result).
+	eval(case(NewLabelTerm, Conditions), Result),!.
 % E-Variant - NOTE: Isn't this a problematic evaluation rule because it can
 % 					never end witha value, based on the grammar?
 % The Small Step version.
@@ -188,49 +188,49 @@ eval(var(Label=Term), Result) :-
 		Result = var(Label=Term);
 		% If not, do another level of evaluation.
 		eval(Term, NewTerm),
-		eval(var(Label=NewTerm),Result)).
+		eval(var(Label=NewTerm),Result)),!.
 % The Big Step Version.
 % eval(var(Label=Term), var(Label=Val)) :-
-% 	eval_if_not_value(Term,Val).
+% 	eval_if_not_value(Term,Val),!.
 
 
 /* --- Exceptions --- */
 /* - Errors w/o values - */
 % E-AppError1
-eval([error, _], error).
+eval([error, _], error),!.
 % E-AppError2
-eval([_, error], error).
+eval([_, error], error),!.
 /* - Error Handling - */
 % E-TryV
 eval(try(Val1, _), Val1) :-
-	is_value(Val1).
+	is_value(Val1),!.
 % E-TryError
 eval(try(error, Term2), Result) :-
-	 eval_if_not_value(Term2, Result).
+	 eval_if_not_value(Term2, Result),!.
 % E-Try
 eval(try(Term1, Term2), Result) :-
 	 eval(Term1, New1),
-	 eval(try(New1, Term2), Result).
+	 eval(try(New1, Term2), Result),!.
 /* - Raising Exceptions - */
 % E-AppRaise1
 eval([raise(Val1), _], raise(Val1)) :-
-	is_value(Val1).
+	is_value(Val1),!.
 % E-AppRaise2
 eval([Val1, raise(Val2)], raise(Val2)) :-
 	is_value(Val1),
-	is_value(Val2).
+	is_value(Val2),!.
 % E-Raise - Sort of Big Step version.
 eval(raise(Term), Result) :-
 	eval(Term, NewTerm),
 	(is_value(NewTerm) ->
 		Result = raise(NewTerm);
-		eval(raise(NewTerm), Result)).
+		eval(raise(NewTerm), Result)),!.
 % E-RaiseRaise
-eval(raise(raise(Val)), raise(Val)) :- is_value(Val).
+eval(raise(raise(Val)), raise(Val)) :- is_value(Val),!.
 % E-TryRaise
 eval(try(raise(Val), TryTerm), Result) :-
 	is_value(Val),
-	eval([TryTerm, Val], Result).
+	eval([TryTerm, Val], Result),!.
 
 /* --- Basic Lambda Calculus Evaluation --- */
 % E-APP1
@@ -238,14 +238,14 @@ eval([Term1,Term2], Result) :-
 	is_not_value(Term1),
 	is_not_value(Term2),
 	eval(Term1,New1),
-	eval([New1,Term2], Result).
+	eval([New1,Term2], Result),!.
 
 % E-APP2
 eval([Val,Term], Result) :-
 	is_value(Val),
 	is_not_value(Term),
 	eval(Term,NewTerm),
-	eval([Val,NewTerm], Result).
+	eval([Val,NewTerm], Result),!.
 
 % E-AppAbs
 eval([Abs,Val],Result) :-
