c07d32121f1ffb4a82428283f543267f3d9e8dbe
JesseHarder <jhharder@scu.edu>
2017-12-01 14:01:32 -0800

    Continued aesthetic improvements.

--- eval.pl
+++ eval.pl
@@ -39,8 +39,8 @@ eval(ifte(Term1, Term2, Term3),Result) :-
 % E-PredZero
 eval(pred(0), 0) :- !.
 % E-PredSucc
-eval(pred(succ(X)), X) :-
-	is_natural_value(X),!.
+eval(pred(succ(Term)), Term) :-
+	is_natural_value(Term),!.
 % E-Succ
 eval(succ(Term), Result) :-
 	eval(Term, NewTerm),
@@ -92,32 +92,32 @@ eval(proj(tuple(List), Index), Result) :-
 	is_list(List), length(List, Len), Index >= 1, Index =< Len,
 	ith_elm(Index, List, Result),!.
 % E-Proj
-eval(proj(tuple(List),Index), Result) :-
+eval(proj(tuple(List), Index), Result) :-
 	eval(tuple(List), NewTuple),
-	eval(proj(NewTuple,Index), Result),!.
+	eval(proj(NewTuple, Index), Result),!.
 % E-Tuple - This is sort of the Big-Step version of this.
 eval(tuple(List), tuple(Vals)) :-
 	is_not_value(tuple(List)),
-	maplist(eval_if_not_value,List,Vals),!.
+	maplist(eval_if_not_value, List, Vals),!.
 
 
 /* --- Records --- */
 % E-ProjRecord
-eval(proj(record(List),Label),Result) :-
+eval(proj(record(List), Label),Result) :-
 	is_value(record(List)),
 	is_list(List), string(Label), % Sanity Check
-	member(Label=Result,List),!.
+	member(Label=Result, List),!.
 % E-Proj
-eval(proj(record(List),Label), Result) :-
+eval(proj(record(List), Label), Result) :-
 	is_not_value(record(List)),	% Sanity check.
 	eval(record(List), NewRecord),
-	eval(proj(NewRecord,Label), Result),!.
+	eval(proj(NewRecord, Label), Result),!.
 % E-Rcd
 eval(record(List), record(NewList)) :-
 	is_not_value(record(List)),
-	record_parts(record(List),Labels,Terms),
-	maplist(eval_if_not_value,Terms,Vals),
-	record_parts(record(NewList),Labels,Vals),!.
+	record_parts(record(List), Labels, Terms),
+	maplist(eval_if_not_value, Terms, Vals),
+	record_parts(record(NewList), Labels, Vals),!.
 
 
 /* --- Lists --- */
@@ -235,28 +235,28 @@ eval(try(raise(Val), TryTerm), Result) :-
 
 /* --- Basic Lambda Calculus Evaluation --- */
 % E-APP1
-eval([Term1,Term2], Result) :-
+eval([Term1, Term2], Result) :-
 	is_not_value(Term1),
 	is_not_value(Term2),
-	eval(Term1,New1),
-	eval([New1,Term2], Result),!.
+	eval(Term1, New1),
+	eval([New1, Term2], Result),!.
 
 % E-APP2
 eval([Val,Term], Result) :-
 	is_value(Val),
 	is_not_value(Term),
-	eval(Term,NewTerm),
-	eval([Val,NewTerm], Result),!.
+	eval(Term, NewTerm),
+	eval([Val, NewTerm], Result),!.
 
 % E-AppAbs
-eval([Abs,Val],Result) :-
+eval([Abs, Val],Result) :-
 	is_lambda(Abs),
 	is_value(Val),
-	apply(Abs,Val,ApResult),
-	eval_if_not_value(ApResult,Result),!.
+	apply(Abs, Val, ApResult),
+	eval_if_not_value(ApResult, Result),!.
 
-eval([Abs,Val|OtherTerms],Result) :-
-	length([Abs,Val|OtherTerms],Len), Len > 2,
-	eval([Abs,Val],NewTerm),
-	eval([NewTerm|OtherTerms],MidResult),
-	eval_if_not_value(MidResult,Result),!.
+eval([Abs, Val|OtherTerms], Result) :-
+	length([Abs, Val|OtherTerms], Len), Len > 2,
+	eval([Abs, Val], NewTerm),
+	eval([NewTerm|OtherTerms], MidResult),
+	eval_if_not_value(MidResult, Result),!.
