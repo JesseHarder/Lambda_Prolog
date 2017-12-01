3e7e7303f29f659d97e8baf043e1419e103c5587
JesseHarder <jhharder@scu.edu>
2017-11-30 18:48:54 -0800

    Minor tweak.

--- type_tests.pl
+++ type_tests.pl
@@ -1,7 +1,7 @@
 :- [types, util/plists].
 /* Print Controls */
-should_log_between_test_types.
-should_log_between_tests.
+should_log_between_test_types :- fail.
+should_log_between_tests :- fail.
 write_btt(T) :- (should_log_between_test_types -> write(T); true).
 write_bt(T) :- (should_log_between_tests -> write(T); true).
 
