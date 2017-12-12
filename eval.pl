34e7d0069d88e9bcdc4850d7eb4fa89b89cd2d1a
JesseHarder <jhharder@scu.edu>
2017-12-11 19:24:14 -0800

    Removed ! from end of eval and eval_ss rules.

--- eval.pl
+++ eval.pl
@@ -15,51 +15,51 @@ eval(Val, Val) :- is_value(Val).
 /* --- Booelean Evaluation --- */
 %E-IfTrue
 eval(ifte(tru, Term2, _) ,Result) :-
-	eval(Term2,Result),!.
+	eval(Term2,Result).
 %E-IfFalse
 eval(ifte(fls, _, Term3), Result) :-
-	eval(Term3, Result),!.
+	eval(Term3, Result).
 %E-If
 eval(ifte(Term1, Term2, Term3), Result) :-
 	eval(Term1, New1),
-	eval(ifte(New1, Term2, Term3), Result),!.
+	eval(ifte(New1, Term2, Term3), Result).
 
 
 /* --- Natural Number Evaluation --- */
 % E-PredZero
-eval(pred(0), 0) :- !.
+eval(pred(0), 0).
 % E-PredSucc
 eval(pred(succ(Term)), Term) :-
-	is_natural_value(Term),!.
+	is_natural_value(Term).
 % E-Succ
 eval(succ(Term), Result) :-
 	eval(Term, NewTerm),
-	eval(succ(NewTerm), Result),!.
+	eval(succ(NewTerm), Result).
 % E-Pred
 eval(pred(Term), Result) :-
 	eval(Term, NewTerm),
-	eval(pred(NewTerm), Result),!.
+	eval(pred(NewTerm), Result).
 % E-IsZeroZero
-eval(iszero(0), tru) :- !.
+eval(iszero(0), tru).
 % E-IsZeroSucc
 eval(iszero(succ(X)), fls) :-
-	is_natural_value(X),!.
+	is_natural_value(X).
 % E-IsZero
 eval(iszero(Term), Result) :-
 	is_not_value(Term),
 	eval(Term, NewTerm),
-	eval(iszero(NewTerm),Result),!.
+	eval(iszero(NewTerm),Result).
 
 
 /* --- Sequences with Unit type ---*/
 % Sequence using lambda calculus.
 eval(seq([Term1, Term2]), Result) :-
 	apply(lam(reserved_sequence_atom_name:'Unit', Term2), Term1, MidResult),
-	eval(MidResult, Result),!.
+	eval(MidResult, Result).
 eval(seq([Term1, Term2 | OtherTerms]), Result) :-
 	length([Term1, Term2 | OtherTerms], Len), Len > 2,
 	eval(seq([Term1, Term2]), MidResult),
-	eval(seq([MidResult | OtherTerms]), Result),!.
+	eval(seq([MidResult | OtherTerms]), Result).
 
 
 /* --- Let --- */
@@ -68,12 +68,12 @@ eval(seq([Term1, Term2 | OtherTerms]), Result) :-
 eval(let(X=Val, Term2), Result) :-
 	atom(X), is_value(Val),
 	substitute(X, Val, Term2, New2),
-	eval(New2, Result),!.
+	eval(New2, Result).
 % E-Let
 eval(let(X=Term1, Term2), Result) :-
 	atom(X), is_not_value(Term1),
 	eval(Term1, New1),
-	eval(let(X=New1, Term2), Result),!.
+	eval(let(X=New1, Term2), Result).
 
 
 /* --- Tuples --- */
@@ -82,15 +82,15 @@ eval(proj(tuple(List), Index), Result) :-
 	is_value(tuple(List)),
 	% Check that List is a non-empty list.
 	is_list(List), length(List, Len), Index >= 1, Index =< Len,
-	ith_elm(Index, List, Result),!.
+	ith_elm(Index, List, Result).
 % E-Proj
 eval(proj(tuple(List), Index), Result) :-
 	eval(tuple(List), NewTuple),
-	eval(proj(NewTuple, Index), Result),!.
+	eval(proj(NewTuple, Index), Result).
 % E-Tuple - This is sort of the Big-Step version of this.
 eval(tuple(List), tuple(Vals)) :-
 	is_not_value(tuple(List)),
-	maplist(eval, List, Vals),!.
+	maplist(eval, List, Vals).
 
 
 /* --- Records --- */
@@ -98,18 +98,18 @@ eval(tuple(List), tuple(Vals)) :-
 eval(proj(record(List), Label),Result) :-
 	is_value(record(List)),
 	is_list(List), string(Label), % Sanity Check
-	member(Label=Result, List),!.
+	member(Label=Result, List).
 % E-Proj
 eval(proj(record(List), Label), Result) :-
 	is_not_value(record(List)),	% Sanity check.
 	eval(record(List), NewRecord),
-	eval(proj(NewRecord, Label), Result),!.
+	eval(proj(NewRecord, Label), Result).
 % E-Rcd
 eval(record(List), record(NewList)) :-
 	is_not_value(record(List)),
 	record_parts(record(List), Labels, Terms),
 	maplist(eval, Terms, Vals),
-	record_parts(record(NewList), Labels, Vals),!.
+	record_parts(record(NewList), Labels, Vals).
 
 
 /* --- Variant Types: Case ---
@@ -128,53 +128,53 @@ eval(case(var(Label=Val), Conditions), Result) :-
 	is_value(Val),
 	member(var(Label=Var)->CondTerm, Conditions),
 	substitute(Var, Val, CondTerm, NewTerm),
-	eval(NewTerm, Result),!.
+	eval(NewTerm, Result).
 % E-Case
 eval(case(var(Label=Term), Conditions), Result) :-
 	eval(var(Label=Term), NewLabelTerm),
-	eval(case(NewLabelTerm, Conditions), Result),!.
+	eval(case(NewLabelTerm, Conditions), Result).
 % E-Variant
 eval(var(Label=Term), var(Label=Val)) :-
 	is_not_value(Term),
-	eval(Term,Val),!.
+	eval(Term,Val).
 
 
 /* --- Lists --- */
 % E-Cons1
 eval(cons(Term1, Term2), Result) :-
 	eval(Term1, New1),
-	eval(cons(New1, Term2), Result),!.
+	eval(cons(New1, Term2), Result).
 % E-Cons2
 eval(cons(Val1, Term2), Result) :-
 	is_value(Val1),
 	eval(Term2, New2),
-	eval(cons(Val1, New2), Result),!.
+	eval(cons(Val1, New2), Result).
 % E-IsNilNil
 eval(isnil(nil), tru).
 % E-IsNilCons
 eval(isnil(cons(V1, V2)), fls) :-
 	is_value(V1),
-	is_value(V2),!.
+	is_value(V2).
 % E-IsNil
 eval(isnil(Term), Result) :-
 	eval(Term, NewTerm),
-	eval(isnil(NewTerm), Result),!.
+	eval(isnil(NewTerm), Result).
 % E-HeadCons
 eval(head(cons(V1, V2)), V1) :-
 	is_value(V1),
-	is_value(V2),!.
+	is_value(V2).
 % E-Head
 eval(head(Term), Result) :-
 	eval(Term, NewTerm),
-	eval(head(NewTerm), Result),!.
+	eval(head(NewTerm), Result).
 % E-TailCons
 eval(tail(cons(V1, V2)), V2) :-
 	is_value(V1),
-	is_value(V2),!.
+	is_value(V2).
 % E-Tail
 eval(tail(Term), Result) :-
 	eval(Term, NewTerm),
-	eval(tail(NewTerm), Result),!.
+	eval(tail(NewTerm), Result).
 
 
 /* --- Exceptions --- */
@@ -186,32 +186,32 @@ eval([_, error], error).
 /* - Error Handling - */
 % E-TryV
 eval(try(Val1, _), Val1) :-
-	is_value(Val1),!.
+	is_value(Val1).
 % E-TryError
 eval(try(error, Term2), Result) :-
-	 eval(Term2, Result),!.
+	 eval(Term2, Result).
 % E-Try
 eval(try(Term1, Term2), Result) :-
 	 eval(Term1, New1),
-	 eval(try(New1, Term2), Result),!.
+	 eval(try(New1, Term2), Result).
 /* - Raising Exceptions - */
 % E-AppRaise1
 eval([raise(Val1), _], raise(Val1)) :-
-	is_value(Val1),!.
+	is_value(Val1).
 % E-AppRaise2
 eval([Val1, raise(Val2)], raise(Val2)) :-
 	is_value(Val1),
-	is_value(Val2),!.
+	is_value(Val2).
 % E-Raise - Sort of Big Step version.
 eval(raise(Term), raise(Val)) :-
 	is_not_value(Term),
-	eval(Term, Val),!.
+	eval(Term, Val).
 % E-RaiseRaise
-eval(raise(raise(Val)), raise(Val)) :- is_value(Val),!.
+eval(raise(raise(Val)), raise(Val)) :- is_value(Val).
 % E-TryRaise
 eval(try(raise(Val), TryTerm), Result) :-
 	is_value(Val),
-	eval([TryTerm, Val], Result),!.
+	eval([TryTerm, Val], Result).
 
 /* --- Fix Operator --- */
 % NOTE: The following does not work for mor than one itteration of fix. It
@@ -234,24 +234,24 @@ eval([Term1, Term2], Result) :-
 	is_not_value(Term1),
 	% is_not_value(Term2),
 	eval(Term1, New1),
-	eval([New1, Term2], Result),!.
+	eval([New1, Term2], Result).
 
 % E-APP2
 eval([Val, Term2], Result) :-
 	is_value(Val),
 	is_not_value(Term2),
 	eval(Term2, New2),
-	eval([Val, New2], Result),!.
+	eval([Val, New2], Result).
 
 % E-AppAbs
 eval([Abs, Val],Result) :-
 	is_lambda(Abs),
 	is_value(Val),
 	apply(Abs, Val, ApResult),
-	eval(ApResult, Result),!.
+	eval(ApResult, Result).
 
 eval([Abs, Val|OtherTerms], Result) :-
 	length([Abs, Val|OtherTerms], Len), Len > 2,
 	eval([Abs, Val], NewTerm),
 	eval([NewTerm|OtherTerms], MidResult),
-	eval(MidResult, Result),!.
+	eval(MidResult, Result).
--- eval_ss.pl
+++ eval_ss.pl
@@ -17,42 +17,42 @@ eval(Term, Val) :-
 
 /* --- Booelean Evaluation --- */
 %E-IfTrue
-eval_ss(ifte(tru, Term2, _), Term2) :- !.
+eval_ss(ifte(tru, Term2, _), Term2).
 %E-IfFalse
-eval_ss(ifte(fls, _, Term3), Term3) :- !.
+eval_ss(ifte(fls, _, Term3), Term3).
 %E-If
 eval_ss(ifte(Term1, Term2, Term3), ifte(New1, Term2, Term3)) :-
-	eval_ss(Term1, New1),!.
+	eval_ss(Term1, New1).
 
 
 /* --- Natural Number Evaluation --- */
 % E-PredZero
-eval_ss(pred(0), 0) :- !.
+eval_ss(pred(0), 0).
 % E-PredSucc
 eval_ss(pred(succ(Term)), Term) :-
-	is_natural_value(Term),!.
+	is_natural_value(Term).
 % E-Succ
 eval_ss(succ(Term),succ(NewTerm)) :-
-	eval_ss(Term, NewTerm),!.
+	eval_ss(Term, NewTerm).
 % E-Pred
 eval_ss(pred(Term), pred(NewTerm)) :-
-	eval_ss(Term, NewTerm),!.
+	eval_ss(Term, NewTerm).
 % E-IsZeroZero
-eval_ss(iszero(0), tru) :- !.
+eval_ss(iszero(0), tru).
 % E-IsZeroSucc
 eval_ss(iszero(succ(X)), fls) :-
-	is_natural_value(X),!.
+	is_natural_value(X).
 % E-IsZero
 eval_ss(iszero(Term), iszero(NewTerm)) :-
-	eval_ss(Term, NewTerm),!.
+	eval_ss(Term, NewTerm).
 
 
 /* --- Sequences with Unit type ---*/
 eval_ss(seq([Term1, Term2]), Result) :-
-	apply(lam(x:'Unit', Term2), Term1, Result),!.
+	apply(lam(x:'Unit', Term2), Term1, Result).
 eval_ss(seq([Term1, Term2 | OtherTerms]), seq([NewTerm | OtherTerms])) :-
 	length([Term1, Term2 | OtherTerms], Len), Len > 2,
-	eval_ss(seq([Term1, Term2]), NewTerm),!.
+	eval_ss(seq([Term1, Term2]), NewTerm).
 
 
 /* --- Let --- */
@@ -60,11 +60,11 @@ eval_ss(seq([Term1, Term2 | OtherTerms]), seq([NewTerm | OtherTerms])) :-
 % E-LetV
 eval_ss(let(X=Val, Term2), New2) :-
 	atom(X), is_value(Val),
-	substitute(X, Val, Term2, New2),!.
+	substitute(X, Val, Term2, New2).
 % E-Let
 eval_ss(let(X=Term1, Term2), let(X=New1, Term2)) :-
 	atom(X), is_not_value(Term1),
-	eval_ss(Term1, New1),!.
+	eval_ss(Term1, New1).
 
 
 /* --- Tuples --- */
@@ -73,10 +73,10 @@ eval_ss(proj(tuple(List), Index), Result) :-
 	is_value(tuple(List)),
 	% Check that List is a non-empty list.
 	is_list(List), length(List, Len), Index >= 1, Index =< Len,
-	ith_elm(Index, List, Result),!.
+	ith_elm(Index, List, Result).
 % E-Proj
 eval_ss(proj(tuple(List), Index), proj(NewTuple, Index)) :-
-	eval_ss(tuple(List), NewTuple),!.
+	eval_ss(tuple(List), NewTuple).
 % E-Tuple - This is sort of the Big-Step version of this.
 eval_ss(tuple(List), tuple(NewList)) :-
 	is_not_value(tuple(List)),
@@ -88,16 +88,16 @@ eval_ss(tuple(List), tuple(NewList)) :-
 eval_ss(proj(record(List), Label), Result) :-
 	is_value(record(List)),
 	is_list(List), string(Label), % Sanity Check
-	member(Label=Result, List),!.
+	member(Label=Result, List).
 % E-Proj
 eval_ss(proj(record(List), Label), proj(NewRecord, Label)) :-
-	eval_ss(record(List), NewRecord),!.
+	eval_ss(record(List), NewRecord).
 % E-Rcd
 eval_ss(record(List), record(NewList)) :-
 	is_not_value(record(List)),
 	record_parts(record(List), Labels, Terms),
 	eval_first_non_value(Terms, NewTerms),
-	record_parts(record(NewList), Labels, NewTerms),!.
+	record_parts(record(NewList), Labels, NewTerms).
 
 
 /* --- Variant Types: Case ---
@@ -115,80 +115,80 @@ eval_ss(case(var(Label=Val), Conditions), Result) :-
 	string(Label),	% Sanity check.
 	is_value(Val),
 	member(var(Label=Var)->CondTerm, Conditions),
-	substitute(Var, Val, CondTerm, Result),!.
+	substitute(Var, Val, CondTerm, Result).
 % E-Case
 eval_ss(case(var(Label=Term), Conditions), case(NewLabelTerm, Conditions)) :-
-	eval_ss(var(Label=Term), NewLabelTerm),!.
+	eval_ss(var(Label=Term), NewLabelTerm).
 % E-Variant
 eval_ss(var(Label=Term), var(Label=NewTerm)) :-
-	eval_ss(Term,NewTerm),!.
+	eval_ss(Term,NewTerm).
 
 
 /* --- Lists --- */
 % E-Cons1
 eval_ss(cons(Term1, Term2), cons(New1, Term2)) :-
-	eval_ss(Term1, New1),!.
+	eval_ss(Term1, New1).
 % E-Cons2
 eval_ss(cons(Val1, Term2), cons(Val1, New2)) :-
 	is_value(Val1),
-	eval_ss(Term2, New2),!.
+	eval_ss(Term2, New2).
 % E-IsNilNil
 eval_ss(isnil(nil), tru).
 % E-IsNilCons
 eval_ss(isnil(cons(V1, V2)), fls) :-
 	is_value(V1),
-	is_value(V2),!.
+	is_value(V2).
 % E-IsNil
 eval_ss(isnil(Term), isnil(NewTerm)) :-
-	eval_ss(Term, NewTerm),!.
+	eval_ss(Term, NewTerm).
 % E-HeadCons
 eval_ss(head(cons(V1, V2)), V1) :-
 	is_value(V1),
-	is_value(V2),!.
+	is_value(V2).
 % E-Head
 eval_ss(head(Term), head(NewTerm)) :-
-	eval_ss(Term, NewTerm),!.
+	eval_ss(Term, NewTerm).
 % E-TailCons
 eval_ss(tail(cons(V1, V2)), V2) :-
 	is_value(V1),
-	is_value(V2),!.
+	is_value(V2).
 % E-Tail
 eval_ss(tail(Term), tail(NewTerm)) :-
-	eval_ss(Term, NewTerm),!.
+	eval_ss(Term, NewTerm).
 
 
 /* --- Exceptions --- */
 /* - Errors w/o values - */
 % E-AppError1
-eval_ss([error, _], error) :- !.
+eval_ss([error, _], error).
 % E-AppError2
-eval_ss([_, error], error) :- !.
+eval_ss([_, error], error).
 /* - Error Handling - */
 % E-TryV
 eval_ss(try(Val1, _), Val1) :-
-	is_value(Val1),!.
+	is_value(Val1).
 % E-TryError
-eval_ss(try(error, Term2), Term2) :- !.
+eval_ss(try(error, Term2), Term2).
 % E-Try
 eval_ss(try(Term1, Term2), try(New1, Term2)) :-
-	 eval_ss(Term1, New1),!.
+	 eval_ss(Term1, New1).
 /* - Raising Exceptions - */
 % E-AppRaise1
 eval_ss([raise(Val1), _], raise(Val1)) :-
-	is_value(Val1),!.
+	is_value(Val1).
 % E-AppRaise2
 eval_ss([Val1, raise(Val2)], raise(Val2)) :-
 	is_value(Val1),
-	is_value(Val2),!.
+	is_value(Val2).
 % E-Raise - Sort of Big Step version.
 eval_ss(raise(Term), raise(NewTerm)) :-
-	eval_ss(Term, NewTerm),!.
+	eval_ss(Term, NewTerm).
 % E-RaiseRaise
 eval_ss(raise(raise(Val)), raise(Val)) :-
-	is_value(Val),!.
+	is_value(Val).
 % E-TryRaise
 eval_ss(try(raise(Val), TryTerm), [TryTerm, Val]) :-
-	is_value(Val),!.
+	is_value(Val).
 
 
 /* --- Fix Operator --- */
@@ -208,19 +208,21 @@ eval_ss(fix(Term), fix(NewTerm)) :-
 /* --- Basic Lambda Calculus Evaluation --- */
 % E-APP1
 eval_ss([Term1, Term2], [New1, Term2]) :-
-	eval_ss(Term1, New1),!.
+	eval_ss(Term1, New1).
 % E-APP2
 eval_ss([Val, Term2], [Val, New2]) :-
-	eval_ss(Term2, New2),!.
+	eval_ss(Term2, New2).
+
 % E-AppAbs
 eval_ss([Abs, Val], Result) :-
 	is_lambda(Abs),
 	is_value(Val),
-	apply(Abs, Val, Result),!.
 % E-AppAbsRecurse
+	apply(Abs, Val, Result).
+
 eval_ss([Abs, Val|OtherTerms], [NewTerm|OtherTerms]) :-
 	length([Abs, Val|OtherTerms], Len), Len > 2,
-	eval_ss([Abs, Val], NewTerm),!.
+	eval_ss([Abs, Val], NewTerm).
 
 /***** Helper Functions *****/
 
